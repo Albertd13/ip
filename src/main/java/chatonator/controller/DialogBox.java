706d47e1966d03aa19b060d906754a6d7ec1a5bc
Albertd13 <albert.ding.13@gmail.com>
2025-09-13 17:49:03 +0800

    Refine GUI aesthetics

--- src/main/java/chatonator/controller/DialogBox.java
+++ src/main/java/chatonator/controller/DialogBox.java
@@ -7,12 +7,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's face
@@ -49,7 +54,17 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        var db = new DialogBox(text, img);
+        db.setBackground(
+                new Background(
+                        new BackgroundFill(
+                                Color.web("0x8CFFDB"),
+                                CornerRadii.EMPTY,
+                                Insets.EMPTY
+                        )
+                )
+        );
+        return db;
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
